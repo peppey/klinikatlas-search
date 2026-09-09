cd731fb295e8eebb171e9ce829bc21545faf2b85
Sebastian Hümpfner <sebastian.huempfner@belucon.de>
2026-09-09 21:01:28 +0200

    tried testing out nominatim for finding coordinates for city names, postal codes. api requests currently yield 403 responses so i will need to lookup their exact usage requirements

--- notebooks/locations_ner.py
+++ notebooks/locations_ner.py
@@ -1,3 +1,6 @@
+import requests
+nominatim_search_endpoint = 'https://nominatim.openstreetmap.org/search?'
+
 
 def extract_locations(texts, model):
     locations = []
@@ -7,3 +10,14 @@ def extract_locations(texts, model):
             if ent.label_ == 'LOC':
                 locations.append(ent.text)
     return locations
+
+def load_coordinates(city_name = None, plz=None):
+    query_params = {"country": "Deutschland"}
+    if city_name:
+        query_params["city"] = city_name
+
+    if plz: 
+        query_params["postalcode"] = plz
+
+    response = requests.get(f"{nominatim_search_endpoint}q={city_name}")
+    return response
